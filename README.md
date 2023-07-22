da77b06ac1c8f3215118f0429afd50c04d1e7c35
oayomide <onigbindeayomide@gmail.com>
2023-07-22 22:32:23 +0200

    cleanup and basic functionality now  w🎉

---
+++ README.md
@@ -0,0 +1,32 @@
+### DUGBE: A simple tool to backup Railway.app hosted Databases
+
+Dugbe is a simple tool to backup Railway.app hosted Databases.
+
+Railway.app is a great infrastructure platform but at the moment, your best bet backing up a database is connecting AWS. 
+Dugbe aims to make this process easier by providing a simple CLI tool to backup your data. 
+
+While this has not been battle tested as much as I would like, I currently use it to backup my [Orchdio](https://orchdio.com) databases.
+I'd be glad to take a look at any issues you may have while trying to use the basic functionality ——backup some database.
+
+
+### What do you need?
+ - A Railway.app account.
+ - A project with a database. For now, its recommended to pass a project that has just 1 database plugin installed. This is because
+   Dugbe will backup the first DB it gets back from the API.
+ - An API token.  You can check the docs [here](https://docs.railway.app/reference/public-api) to see how to you can get started with API.
+ - `pg_dump` installed on your machine. You may find [this](https://www.cyberithub.com/how-to-install-pg_dump-and-pg_restore-on-ubuntu-20-04-lts-focal/) helpful on how to see how to install it on your machine.
+
+
+### How to use
+```bash
+# Install dugbe
+Please install by cloning this repo. Then update your environment variables with your Railway.app API token. A sample .env file is provided.
+# Run dugbe
+dugbe backup --output './backup.sql` --service <your railway.app db hosted service> --database <the URL of the db you want to backup>
+```
+
+### What's next?
+ - [ ] Add support for backing up multiple databases.
+ - [ ] Better UI and progress update.
+ - [ ] Env variable support from flag.
+ - [ ] Publish to crates.io
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::path::{Path, PathBuf};
-use clap::{Arg, arg, Command, value_parser};
+use std::ffi::OsStr;
+use std::path::{PathBuf};
+use std::time::Instant;
+use clap::{arg, Command, value_parser};
 use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
 
@@ -36,6 +38,8 @@ struct ProjectInfoResponse {
     data: Project
 }
 fn main() {
+    dotenv::dotenv().ok();
+    println!("🚂 DUGBE: A RAILWAY DB BACKUP TOOL. THIS TOOL IS ALPHA QUALITY. ITS SAFE ENOUGH TO USE BUT ITS NOT BEEN SO BATTLE TESTED FOR THE SCALE OF THE USAGE (PRODUCTION DB BACKUP ON RAILWAY INFRA.). PLEASE REPORT ANY ISSUES AND BE PATIENT WITH ISSUES THAT MAY ARISE.");
     let cmd = clap::Command::new("railway-db-backup")
         .bin_name("railway-db-backup")
         .subcommand_required(true)
@@ -43,13 +47,18 @@ fn main() {
             Command::new("backup")
                 .about("Backup a Railway database")
                 .arg(
-                    arg!(-o --output <PATH> "The final location to dump the backup data")
+                    arg!(-o --output <PATH> "The final location to dump the backup data. Must be an sql file.")
                         .required(true)
                         .value_parser(value_parser!(PathBuf))
                 )
                 .arg(
-                    arg!(-d --database <ID> "The database to backup")
+                    arg!(-s --service <ID> "The service that hosts the database we want to backup.")
                         .required(true)
+                )
+                .arg(
+                    arg!(-d --database <URL> "The database we want to backup.")
+                        .required(true)
+                        .value_parser(value_parser!(String))
                 )
         );
 
@@ -57,22 +66,51 @@ fn main() {
 
     let backup_matches = matches.subcommand_matches("backup").unwrap();
 
-    let db_and_output = match backup_matches {
-        Matches => {
-            let database  = Matches.get_one::<String>("database").expect("Please provide a valid Railway database ID");
-            let output = Matches.get_one::<PathBuf>("output").expect("Please provide a valid output path");
-            println!("Backing up {} to {}", database, output.to_str().unwrap());
-            (database, output)
+    let backup_arguments = match backup_matches {
+        bk_matches => {
+            let service  = bk_matches.get_one::<String>("service").expect("Please provide a valid Railway database ID");
+            let output = bk_matches.get_one::<PathBuf>("output").expect("Please provide a valid output path");
+            let database = bk_matches.get_one::<String>("database").expect("Please provide a valid Railway database URL.");
+            println!("Backing up {} on Railway Service {} to {}", database, service.as_str(), output.to_str().unwrap());
+            (database, service, output)
         }
     };
 
-    let valid_uuid = match uuid::Uuid::parse_str(db_and_output.0.as_str()) {
+    let valid_uuid = match uuid::Uuid::parse_str(backup_arguments.1.as_str()) {
         Ok(uuid) => uuid,
         Err(_) => {
             println!("Invalid Railway database project ID passed. Please provide a valid Railway database ID");
             std::process::exit(1);
         }
     };
+
+    match backup_arguments.2.as_path().extension().and_then(OsStr::to_str) {
+        Some("sql") => {},
+        _ => {
+            println!("Invalid output file extension. Please provide a valid output file extension. The filename you specified is not an sql file. You specified: {}", backup_arguments.2.as_path().display());
+            std::process::exit(1);
+        }
+    }
+
+    println!("Valid Project Service passed...: {}", valid_uuid);
+    let (database, service, output) = backup_arguments;
+    let database_info = match url::Url::parse(database.as_str()) {
+        Ok(url) => {
+            println!("Valid Database URL passed...: {}", url);
+            url
+        },
+        Err(_) => {
+            println!("Invalid Railway database URL passed. Please provide a valid Railway database URL.");
+            std::process::exit(1);
+        }
+    };
+    println!("Database connection info: {:?}", database_info);
+
+    if database_info.scheme() != "postgresql" {
+        println!("Invalid Railway database URL passed. Please provide a valid Railway database URL.11");
+        std::process::exit(1);
+    }
+
 
     let token = match std::env::var("RAILWAY_API_TOKEN") {
         Ok(token) => token,
@@ -82,8 +120,15 @@ fn main() {
         }
     };
 
-    println!("Valid Database Project UUID passed...: {}\n", valid_uuid);
-    let (database, output) = db_and_output;
+
+    // check if pg_dump is installed. Please make sure pg_dump is installed before running this tool.
+    let pg_dump = match which::which("pg_dump") {
+        Ok(path) => path,
+        Err(_) => {
+            println!("pg_dump not found. Please install pg_dump and try again");
+            std::process::exit(1);
+        }
+    };
 
     let query = format!(r#"
         query project {{
@@ -100,7 +145,7 @@ fn main() {
             }}
           }}
         }}
-    "#, database);
+    "#, service);
     let client = Client::new();
 
     let body = &serde_json::json!({
@@ -109,19 +154,9 @@ fn main() {
         });
 
     let request = client.post("https://backboard.railway.app/graphql/v2")
-        // todo: implement fetching keys from env var
         .header("Content-Type", "application/json")
         .bearer_auth(token)
         .json(body);
-
-    let req_clone = request.try_clone().unwrap().build().unwrap();
-    let headers = req_clone
-        .headers()
-        .clone();
-
-    let resp_body = req_clone
-        .body()
-        .unwrap();
 
     let response = request
         .send()
@@ -137,7 +172,52 @@ fn main() {
         .json::<ProjectInfoResponse>()
         .expect("Failed to parse response from Railway API");
 
-    println!("Response: {:?}", resp_body);
+    let nb_plugins = resp_body.data.project.plugins.edges.len();
+    if nb_plugins > 1 {
+        println!("Warning: {} plugins found. This tool only supports backing up a single database plugin. The first plugin will be used.", nb_plugins);
+        println!("Using plugin: {:?}", resp_body.data.project.plugins.edges.first().unwrap());
+    }
+    let plugin = resp_body.data.project.plugins.edges.first().unwrap();
 
+    println!("Response: {:?}", plugin);
+
+    if plugin.node.name != "postgresql" {
+        println!("Error: The plugin found is not a PostgreSQL database. This tool only supports backing up a PostgreSQL database.");
+        std::process::exit(1);
+    }
+
+    let db_host = database_info.host().unwrap().to_string();
+    let db_port = database_info.port().unwrap_or(5432).to_string();
+    let database_name = database_info.path().trim_start_matches("/");
+
+    let pg_dump_args = vec![
+        "-h", db_host.as_str(),
+        "-p", db_port.as_str(),
+        "-U", "postgres",
+        "-d", database_name,
+        "-f", output.to_str().unwrap()
+    ];
+
+    // start a timer
+    let start = Instant::now();
+    let pg_dump = std::process::Command::new(pg_dump)
+        .args(&pg_dump_args)
+        .env("PGPASSWORD", database_info.password().unwrap())
+        .output()
+        .expect("Failed to run pg_dump");
+let end = start.elapsed();
+
+    let time_spent = format!("{}.{}s", end.as_secs(), end.subsec_millis());
+    if !pg_dump.status.success() {
+        let error = String::from_utf8_lossy(&pg_dump.stderr);
+        println!("Failed to run pg_dump. There seems to be an error connecting to the database. Please check your connection info and try again.");
+        println!("Error: {}", error);
+        println!("Time spent: {}", time_spent);
+        std::process::exit(1);
+    }
+
+
+    println!("pg_dump output: {:?}", pg_dump);
+    println!("Done backing up: Took {}", time_spent);
 }
 
